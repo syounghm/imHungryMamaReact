c87c89a6769861aeea8d34eabcfeee72a6a18ff5
syounghm <stevenyoung@imhungrymama.com>
2023-07-21 20:32:56 -0400

    react implementation start header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Layout, Menu, theme } from "antd";
+import ProductCatalog from "./pages/ProductCatalog";
+
+const { Header, Content, Footer } = Layout;
+function getItem(label, key) {
+  return {
+    label,
+    key,
+  };
+}
+const items = [
+  getItem("Rice & Noodles", "1"),
+  getItem("Sauces", "2"),
+  getItem("Seasonings, Spices, & Broth Mixes", "3"),
+  getItem("Tapioca Products", "4"),
+  getItem("Teas & Coffees", "5"),
+  getItem("Candies", "6"),
+];
 
 function App() {
+  const [type, setType] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "#cc462b",
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          onClick={(item, key) => setType(item.key)}
+          className="menuBar"
+          style={{ backgroundColor: "#cc462b" }}
+          mode="horizontal"
+          // defaultSelectedKeys={["2"]}
+          items={items}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "0 50px",
+        }}
+      >
+        <div className="site-layout-content" style={{}}>
+          Content
+        </div>
+        <ProductCatalog type={type} />
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
   );
 }
-
 export default App;
---
+++ src/pages/ProductCatalog.js
@@ -0,0 +1,6 @@
+import React from "react";
+const ProductCatalog = (props) => {
+  const { type } = props;
+  return <>Hello {`${type}`}</>;
+};
+export default ProductCatalog;
